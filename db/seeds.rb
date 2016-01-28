fb2facf56c2beac2072a100d460efe91b6b03a7a
Jacob Mziya <jacobmziya@gmail.com>
2016-01-28 12:40:20 +0200

    added seed template

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+=begin
+	#code to consider for DB Seed metadata1.7
+unless Rails.env.production?
+  connection = ActiveRecord::Base.connection
+  connection.tables.each do |table|
+    connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+  end
+   
+  # - IMPORTANT: SEED DATA ONLY
+  # - DO NOT EXPORT TABLE STRUCTURES
+  # - DO NOT EXPORT DATA FROM `schema_migrations`
+  sql = File.read('db/openmrs_metadata_1_7.sql')
+  statements = sql.split(/;$/)
+  statements.pop  # the last empty statement
+ 
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
+  end
+end
+=end
