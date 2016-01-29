becf7a8e7f2f00a299ab57fcc8e74165e9060668
mwatha <mwathabwanali@gmail.com>
2016-01-30 01:38:10 +0200

    working on dashbords: 30% done

---
+++ app/models/global_property.rb
@@ -0,0 +1,9 @@
+class GlobalProperty < ActiveRecord::Base
+  self.table_name ='global_property'
+  include Openmrs
+
+  def self.current_health_center
+    Location.find(self.find_by_description('Current.health.center').property_value)
+  end
+
+end
