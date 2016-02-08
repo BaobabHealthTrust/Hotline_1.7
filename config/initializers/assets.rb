a8a9578c1a2701848aef3b59e1f6940fccb52225
mwatha <mwathabwanali@gmail.com>
2016-02-08 11:44:29 +0200

    working on dashbords: 45% done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( user/* )
+Rails.application.config.assets.precompile += %w( touch-fancy.css )
