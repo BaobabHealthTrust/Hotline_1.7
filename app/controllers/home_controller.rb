d2058667d4dfd127e0cf0713af518319f0cedcbd
mwatha <mwathabwanali@gmail.com>
2016-03-10 22:02:41 +0200

    UI changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -37,4 +37,14 @@ class HomeController < ApplicationController
     @names = Location.where("name LIKE '%#{search_string}%'").limit(10).map(&:name).sort
     render :text => "<li>" + @names.map{|n| n } .join("</li><li>") + "</li>"
   end
+
+  def quick_summary
+    @stats = Hash.new(0)
+    @stats['Total clients registered'] = Patient.count
+    @stats['Total clients registered (Women)'] = Patient.where("p.gender = 'F' OR p.gender = 'Female'").joins("INNER JOIN person p ON p.person_id=patient.patient_id").count
+    @stats['Total clients registered (Men)'] = Patient.where("p.gender = 'M' OR p.gender = 'Male'").joins("INNER JOIN person p ON p.person_id=patient.patient_id").count
+
+    render :layout => false
+  end
+
 end
