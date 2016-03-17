e29b7eb1a5fa7c1ede95aca1b74f91b5078016ce
Jacob Mziya <jacobmziya@gmail.com>
2016-03-17 22:12:41 +0200

    Child symptoms and danger added when client less 59months.

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -102,14 +102,26 @@ class EncountersController < ApplicationController
   def new
     @patient = Patient.find(params[:patient_id] || session[:patient_id])
     encounter_type = params[:encounter_type].humanize
-
+    person = Person.find(@patient.patient_id)
+    birthdate = person.birthdate.to_s.split('-')
+    child_age = Time.new(birthdate[0], birthdate[1], birthdate[2], 00, 00, 00)#+59.months
+    child_age = child_age.to_s
+    today = Time.now.to_s
+    @age = today.to_i-child_age.to_i
     case encounter_type
       when 'Pregnancy status'
         @pregnancy_options = concept_set('Pregnancy status')
       when 'Female symptoms'
         @maternal_health_symptoms = concept_set('Maternal health symptoms')
-        @danger_signs = concept_set('Danger signs')
-        @maternal_health_info = concept_set('Maternal health info')
+          #if child
+        if @age <= 5
+          @health_info = concept_set('Child health info')
+          @danger_signs = concept_set('Child danger signs greater zero outcome')
+          #if adult
+        elsif
+          @health_info = concept_set('Maternal health info')
+          @danger_signs = concept_set('Danger signs')
+        end
       when 'Update outcomes'
         @general_outcomes = concept_set('General outcome')
       when 'Reminders'
