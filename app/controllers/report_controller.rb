b21af645231b02d3b1c48ff8a7abde5447aafa80
Jacob Mziya <jacobmziya@gmail.com>
2016-06-23 16:40:06 +0200

    Added school aged children filter.

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -85,29 +85,49 @@ class ReportController < ApplicationController
           when "patient_analysis"
             case @query
               when "demographics"
-                @patient_type       += ["Women", "Non-MNCH", "Children", "All"]
+                @patient_type       += ["Women",
+                                        "Non-MNCH",
+                                        ["Children"],
+                                        "School Aged Children",
+                                        "All"]
                 @grouping           += [["By Week", "week"], ["By Month", "month"]]
                 @destination        = [["",""],["To CSV Format", "csv"], ["To Screen", "screen"]]
 
               when "health_issues"
-                @patient_type       += ["Women", "Non-MNCH", "Children", "All"]
+                @patient_type       += ["Women",
+                                        "Non-MNCH",
+                                        ["Children"],
+                                        "School Aged Children",
+                                        "All"]
                 @grouping           += [["By Week", "week"], ["By Month", "month"]]
                 @health_task         = ["", "Health Symptoms", "Danger Warning Signs",
                                         "Health Information Requested", "Outcomes"]
                 @destination        = [["",""],["To CSV Format", "csv"], ["To Screen", "screen"]]
 
               when "ages_distribution"
-                @patient_type       += ["Women", "Non-MNCH", "Children", "All"]
+                @patient_type       += ["Women",
+                                        "Non-MNCH",
+                                        ["Children"],
+                                        "School Aged Children",
+                                        "All"]
                 @grouping           += [["By Week", "week"], ["By Month", "month"]]
                 @destination        = [["",""],["To CSV Format", "csv"], ["To Screen", "screen"]]
 
               when "patient_activity"
-                @patient_type       += ["Women", "Non-MNCH", "Children", "All"]
+                @patient_type       += ["Women",
+                                        "Non-MNCH",
+                                        ["Children"],
+                                        "School Aged Children",
+                                        "All"]
                 @grouping           += [["By Week", "week"], ["By Month", "month"]]
                 @destination        = [["",""],["To CSV Format", "csv"], ["To Screen", "screen"]]
 
               when "referral_followup"
-                @patient_type       += ["Women", "Non-MNCH", "Children", "All"]
+                @patient_type       += ["Women",
+                                        "Non-MNCH",
+                                        ["Children"],
+                                        "School Aged Children",
+                                        "All"]
                 @outcomes            = ["","REFERRED TO A HEALTH CENTRE",
                                         "REFERRED TO NEAREST VILLAGE CLINIC",
                                         "PATIENT TRIAGED TO NURSE SUPERVISOR",
@@ -125,7 +145,11 @@ class ReportController < ApplicationController
                 @grouping           += [["By Week", "week"], ["By Month", "month"]]
                 @destination         = [["",""],["To CSV Format", "csv"], ["To Screen", "screen"]]
               else
-                @patient_type       += ["Women", "Children", "All"]
+                @patient_type       += ["Women",
+                                        "Non-MNCH",
+                                        ["Children"],
+                                        "School Aged Children",
+                                        "All"]
                 @grouping           += [["By Week", "week"], ["By Month", "month"]]
                 @staff               = [["",""]] + get_staff_members_list + [["All","All"]]
                 @call_type           = ["","Normal", #"Followup","Non-Patient Tips",
