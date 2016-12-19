1ad8172b929b19edd377c57b10a2bafefd011a1e
Jacob Mziya <jacobmziya@gmail.com>
2016-12-19 10:21:40 +0200

    Added dataTable for call history.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,10 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( user/* )
 Rails.application.config.assets.precompile += %w( touch-fancy.css )
-Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.min.css )
+Rails.application.config.assets.precompile += %w( dataTables.bootstrap4.css )
 
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( dataTables/* )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.min.js )
+Rails.application.config.assets.precompile += %w( dataTables.bootstrap4.min.js )
